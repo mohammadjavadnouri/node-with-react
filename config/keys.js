0cd9fc7b9fec76256187edd05a5698090adc43db
mohammadjavadnouri <nouri_mj@yahoo.com>
2024-10-01 12:02:36 +0330

    edit envs

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 //keys.js  figure out what set of credentials to return
 
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prod");
-} else {
+if (process.env.NODE_ENV === "dev") {
   module.exports = require("./dev");
+} else {
+  module.exports = require("./prod");
 }

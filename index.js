47784e9b3dc9777d2f18dc706afc3504b983117d
mohammadjavadnouri <nouri_mj@yahoo.com>
2024-10-07 07:20:49 +0330

    test routing frontend

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ app.use("/api/stripe", billingRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirnamem, "client", "build", "index.html"));
-  });
 }
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirnamem, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 

8bb49a92f202cdff6cf164fedc12bfadc988de67
mohammadjavadnouri <nouri_mj@yahoo.com>
2024-10-07 14:54:56 +0330

    typo

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirnamem, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
